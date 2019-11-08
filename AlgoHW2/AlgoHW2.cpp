7ca3d04f18c4eab06fe6300911822d43ee114497
Collin Broderick <cabroderick@wpi.edu>
2019-11-08 16:56:14 -0500

    Finished assignment!

--- AlgoHW2/AlgoHW2.cpp
+++ AlgoHW2/AlgoHW2.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 double previousTime = 1;
 
+/*
+Collin Broderick, Praise Eteng, Siddhant Banerjee
+Homework questions:
+1.
+The characteristic equation is -1 + 3(3^n)
+The reccurence's order of growth is O(b^n)
+2.
+Lucas is also known for the tower of Hanoi
+3.
+The ratio of successive calculations is the golden ratio (~1.6)
+The order of growth is O(n^2)
+4.
+The sum with the greatest number of combinations is 66, which appeared 1364 times. That sum is the halfway to the maximum value that can be created with the values of the square.
+*/
+
+
 // Gets the user input
 int getInput() {
 	printf("Enter a number for a Lucas sequence: ");
@@ -74,6 +90,8 @@ void count33() {
 // Finds all combinations for a list numbers
 void findCombos() {
 	int nums[16] = { 1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5, 13, 2, 3, 15 };
+	int maxCombo = 0;
+	int maxSum = 0;
 	int results[133]; // Each index in results indicates a possible sum, and the value at that index indicates how many times that sum was achieved
 	for (int i = 0; i < 133; i++) { // Fills results with 0s
 		results[i] = 0;
@@ -90,12 +108,17 @@ void findCombos() {
 			a = a >> 1;
 		}
 		results[total]++; // Indexes the total at array value total, which will indicate that that sum was reached one more time
+		if (results[total] > maxCombo) {
+			maxCombo = results[total];
+			maxSum = total;
+		}
 	}
 	for (int i = 0; i < 133; i++) { // Indexes through results
 		if (results[i] > 0) { // If the given sum appeared at least once
 			printf("%d appeared %d times\n", i, results[i]);
 		}
 	}
+	printf("The greatest number of cominbations was %d, which summed to %d.\n", maxCombo, maxSum);
 }
 
 int main() {
